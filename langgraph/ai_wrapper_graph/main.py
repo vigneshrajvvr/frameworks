e10ceb5c1a2486f55ba03e9f86033f37f277007a
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2025-12-10 17:41:06 +0530

    Hugging Face implementation

--- langgraph/ai_wrapper_graph/main.py
+++ langgraph/ai_wrapper_graph/main.py
@@ -1,9 +1,9 @@
 from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import HumanMessage
 from agent_state import AgentState
 from dotenv import load_dotenv
 from process_node import process_node
 
-load_dotenv()
 graph = StateGraph(AgentState)
 
 graph.add_node("process_node", process_node)
@@ -12,5 +12,8 @@ graph.add_edge("process_node", END)
 
 graph = graph.compile()
 
-graph.invoke({"messages": ["Hello!"]})
+user_input = input("Enter:")
+graph.invoke({
+    "messages": [HumanMessage(content=user_input)]
+})
 
--- langgraph/ai_wrapper_graph/process_node.py
+++ langgraph/ai_wrapper_graph/process_node.py
@@ -1,11 +1,26 @@
-from langchain_openai import ChatOpenAI
 from agent_state import AgentState
 from langchain_core.messages import HumanMessage
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+# from langchain_openai import ChatOpenAI
+import os
+
+load_dotenv()
+
+HF_MODEL_ID=os.getenv("HF_MODEL_ID")
+HF_API_TOKEN=os.getenv("HF_API_TOKEN")
+
+# llm = ChatOpenAI(model="gpt-4o") Requires a paid subscription
+llm = HuggingFaceEndpoint(
+    repo_id=HF_MODEL_ID,
+    huggingfacehub_api_token=HF_API_TOKEN,
+    task="text-generation"
+)
+
+chat_model = ChatHuggingFace(llm=llm)
 
 def process_node(state: AgentState) -> AgentState:
     """To get the openai api responses"""
-    llm = ChatOpenAI(model="gpt-4o")
-    user_input = input("Enter:")
-    response=llm.invoke({"message": [HumanMessage(content=user_input)]})
+    response=chat_model.invoke(state["messages"])
     print(f"\nAI: {response.content}")
     return state
